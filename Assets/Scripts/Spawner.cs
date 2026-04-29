9e6c8e40fec5473871be146865308d9ba78baf72
AddKhan <adrinakhan2006@gmail.com>
2026-04-29 19:20:04 -0400

    updated spawner script

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,34 +8,37 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
 {
     public NetworkPrefabRef playerPrefab;
     private NetworkRunner _runner;
-    
     private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
 
     async void Start()
     {
-        _runner = gameObject.GetComponent<NetworkRunner>();
+        // 1. get/add runner
+        _runner = GetComponent<NetworkRunner>();
         if (_runner == null) _runner = gameObject.AddComponent<NetworkRunner>();
         
+        // 2. revent "NetworkRunner should not be reused" err
+        if (_runner.IsRunning) return;
+
         _runner.ProvideInput = true;
 
+        // 3. start scene
         await _runner.StartGame(new StartGameArgs {
             GameMode = GameMode.Shared,
-            SessionName = "CarRaceRoom",
+            SessionName = "MultiAgentRoom",
             SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
         });
     }
 
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-        if (runner.IsSharedModeMasterClient || runner.IsServer)
+        if (runner.IsSharedModeMasterClient)
         {
-            Debug.Log($"Player {player} joined. Spawning car...");
-            
             if (!_spawnedCharacters.ContainsKey(player))
             {
-                Vector3 spawnPosition = new Vector3(player.RawEncoded % 3 * 2, 1, 0); 
-                
-                NetworkObject networkPlayerObject = runner.Spawn(playerPrefab, spawnPosition, Quaternion.identity, player);
+                Debug.Log($"Spawning car for Player: {player}");
+                // spawn offset
+                Vector3 spawnPos = new Vector3(player.RawEncoded * 3, 1, 0);
+                NetworkObject networkPlayerObject = runner.Spawn(playerPrefab, spawnPos, Quaternion.identity, player);
                 _spawnedCharacters.Add(player, networkPlayerObject);
             }
         }
@@ -50,13 +53,14 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
         }
     }
 
+    // empty imps
     public void OnInput(NetworkRunner runner, NetworkInput input) { }
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
     public void OnConnectedToServer(NetworkRunner runner) { }
     public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    // public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
     public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
     public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
@@ -67,4 +71,9 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
     public void OnSceneLoadStart(NetworkRunner runner) { }
     public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
     public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+        throw new NotImplementedException();
+    }
 }
