7b2516c1d7d0cdf9c3eb23416055cf85e42d95b2
AddKhan <adrinakhan2006@gmail.com>
2026-04-29 21:16:42 -0400

    update NetworkRunner obj to fix null ref when trying to spawn a car

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,57 +10,66 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
     private NetworkRunner _runner;
     private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
 
-    async void Start()
+    private void OnGUI()
     {
-        // 1. get/add runner
-        _runner = GetComponent<NetworkRunner>();
-        if (_runner == null) _runner = gameObject.AddComponent<NetworkRunner>();
-        
-        // 2. revent "NetworkRunner should not be reused" err
-        if (_runner.IsRunning) return;
+        if (_runner == null)
+        {
+            if (GUI.Button(new Rect(10, 10, 200, 40), "Host Session"))
+            {
+                StartGame(GameMode.Shared);
+            }
+            if (GUI.Button(new Rect(10, 60, 200, 40), "Join Session"))
+            {
+                StartGame(GameMode.Shared);
+            }
+        }
+    }
 
+    async void StartGame(GameMode mode)
+    {
+        // 1. create runner comp
+        _runner = gameObject.AddComponent<NetworkRunner>();
         _runner.ProvideInput = true;
 
-        // 3. start scene
-        await _runner.StartGame(new StartGameArgs {
-            GameMode = GameMode.Shared,
-            SessionName = "MultiAgentRoom",
-            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        // 2. create scene manager if doesnt exist
+        var sceneManager = gameObject.GetComponent<NetworkSceneManagerDefault>();
+        if (sceneManager == null) sceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>();
+
+        // 3. session start
+        await _runner.StartGame(new StartGameArgs
+        {
+            GameMode = mode,
+            SessionName = "CarRaceRoom",
+            SceneManager = sceneManager
         });
     }
 
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
+        // master client controls car spawn amount
         if (runner.IsSharedModeMasterClient)
         {
-            if (!_spawnedCharacters.ContainsKey(player))
-            {
-                Debug.Log($"Spawning car for Player: {player}");
-                // spawn offset
-                Vector3 spawnPos = new Vector3(player.RawEncoded * 3, 1, 0);
-                NetworkObject networkPlayerObject = runner.Spawn(playerPrefab, spawnPos, Quaternion.identity, player);
-                _spawnedCharacters.Add(player, networkPlayerObject);
-            }
+            Debug.Log("Spawning car for player: " + player);
+            // spawn car offset
+            Vector3 spawnPos = new Vector3(player.RawEncoded * 5, 2, 0); 
+            runner.Spawn(playerPrefab, spawnPos, Quaternion.identity, player);
         }
     }
 
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
-    {
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) {
         if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
         {
-            runner.Despawn(networkObject);
+            if (networkObject != null) runner.Despawn(networkObject);
             _spawnedCharacters.Remove(player);
         }
     }
-
-    // empty imps
     public void OnInput(NetworkRunner runner, NetworkInput input) { }
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
     public void OnConnectedToServer(NetworkRunner runner) { }
     public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
-    // public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
     public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
     public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
@@ -71,9 +80,4 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
     public void OnSceneLoadStart(NetworkRunner runner) { }
     public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
     public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
-
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-    {
-        throw new NotImplementedException();
-    }
 }
